0e79146c7f69d002cba10dbc92051844854102a0
Nazarí González <nazari.nz@gmail.com>
2017-04-24 23:31:58 +0100

    improved point class with an observable pattern

--- Sources/prime/Point.hx
+++ Sources/prime/Point.hx
@@ -1,18 +1,33 @@
 package prime;
 
 class Point {
-	public var x:Float = 0;
-	public var y:Float = 0;
+	private var _x:Float = 0;
+	private var _y:Float = 0;
+	
+	public var x(get, set):Float;
+	public var y(get, set):Float;
+
+	private var _observer:Point -> Void = function(point:Point) : Void {};
+	private var _isObserved:Bool = false;
+	public var isObserved(get, null):Bool;
+
 
 	public function new(x:Float = 0, y:Float = 0) {
-		this.set(x,y);
+		set(x,y);
+	}
+
+	public function setObserver(cb:Point -> Void) : Void {
+		_isObserved = true;
+		_observer = cb;
+	}
+
+	public function removeObserver() : Void {
+		_isObserved = false;
 	}
 
 	public function set(x:Float, ?y:Float) {
 		this.x = x;
-		if(y == null){
-			this.y = x;
-		}
+		this.y = (y == null) ? x : y;
 	}
 
 	public function clone(point:Point) : Point {
@@ -25,5 +40,33 @@ class Point {
 
 	public function isEqual(point:Point) : Bool {
 		return (x == point.x && y == point.y);
+	}
+
+	function get_x() : Float {
+		return _x;
+	}
+
+	function set_x(value:Float) : Float {
+		_x = value;
+		if(_isObserved)_observer(this);
+		return _x;
+	}
+
+	function get_y() : Float {
+		return _y;
+	}
+
+	function set_y(value:Float) : Float {
+		_y = value;
+		if(_isObserved)_observer(this);
+		return _y;
+	}
+
+	function get_isObserved() : Bool {
+		return _isObserved;
+	}
+
+	function toString() : String {
+		return "Point(" + _x + "," + _y + ")";
 	}
 }
