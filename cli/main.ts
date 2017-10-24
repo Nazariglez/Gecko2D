22a4f43d2e68213dfd7d3d10839d428053da011f
Nazarí González <nazari.nz@gmail.com>
2017-10-24 17:12:57 +0100

    force to use node >= 6

--- cli/main.ts
+++ cli/main.ts
@@ -1,4 +1,11 @@
 ///<reference path="../node_modules/@types/node/index.d.ts" />
 import {run} from './cli';
-run();
+import {nodeVersion} from './cmd/utils';
+import * as colors from 'colors';
+
+if(nodeVersion()[0] < 6){
+    console.error(colors.red("Invalid node version. Must be >= 6.x.x"));
+}else{
+    run();
+}
 
