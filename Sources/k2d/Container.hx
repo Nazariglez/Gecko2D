e4e13a9551ac82ca4fe694048575e8e95d3a0b4d
Nazarí González <nazari.nz@gmail.com>
2017-11-06 22:25:32 +0000

    avoid some extra allocations in local contexts

--- Sources/k2d/Container.hx
+++ Sources/k2d/Container.hx
@@ -5,6 +5,17 @@ import k2d.math.FastFloat;
 
 class Container extends Entity {
     public var children:Array<Entity> = new Array<Entity>();
+    public var sizeByChildren:Bool = true;
+
+    private var _minX:FastFloat;
+    private var _maxX:FastFloat;
+    private var _minY:FastFloat;
+    private var _maxY:FastFloat;
+
+    private var _cMinX:FastFloat;
+    private var _cMaxX:FastFloat;
+    private var _cMinY:FastFloat;
+    private var _cMaxY:FastFloat;
 
     public function addChild(child:Entity) {
         child.parent = this;
@@ -18,27 +29,33 @@ class Container extends Entity {
 
     public override function update(dt:FastFloat) {
         super.update(dt);
-        var minX:FastFloat = Math.POSITIVE_INFINITY;
-        var maxX:FastFloat = Math.NEGATIVE_INFINITY;
-        var minY:FastFloat = Math.POSITIVE_INFINITY;
-        var maxY:FastFloat = Math.NEGATIVE_INFINITY;
+        if(!sizeByChildren){
+            for(child in children){
+                child.update(dt);
+            }
+        } else {
+            _minX = Math.POSITIVE_INFINITY;
+            _maxX = Math.NEGATIVE_INFINITY;
+            _minY = Math.POSITIVE_INFINITY;
+            _maxY = Math.NEGATIVE_INFINITY;
 
-        for(child in children){
-            child.update(dt);
+            for(child in children){
+                child.update(dt);
 
-            var cMinX:FastFloat = child.position.x - child.width * child.anchor.x;
-            var cMaxX:FastFloat = child.position.x + child.width * (1-child.anchor.x);
-            var cMinY:FastFloat = child.position.y - child.height * child.anchor.y;
-            var cMaxY:FastFloat = child.position.y + child.height * (1-child.anchor.y);
+                _cMinX = child.position.x - child.width * child.anchor.x;
+                _cMaxX = child.position.x + child.width * (1-child.anchor.x);
+                _cMinY = child.position.y - child.height * child.anchor.y;
+                _cMaxY = child.position.y + child.height * (1-child.anchor.y);
 
-            if(cMinX < minX)minX = cMinX;
-            if(cMinY < minY)minY = cMinY;
-            if(cMaxX > maxX)maxX = cMaxX;
-            if(cMaxY > maxY)maxY = cMaxY;
+                if(_cMinX < _minX)_minX = _cMinX;
+                if(_cMinY < _minY)_minY = _cMinY;
+                if(_cMaxX > _maxX)_maxX = _cMaxX;
+                if(_cMaxY > _maxY)_maxY = _cMaxY;
+            }
+
+            size.x = _maxX - _minX;
+            size.y = _maxY - _minY;
         }
-
-        size.x = maxX - minX;
-        size.y = maxY - minY;
     }
 
     public override function render(r:Renderer2D) {
