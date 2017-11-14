3a022c0b9ba3b25104fc89442de9eb56fbfca985
Nazarí González <nazari.nz@gmail.com>
2017-11-14 15:49:00 +0000

    finished tweengroups

--- Sources/k2d/tween/TweenGroup.hx
+++ Sources/k2d/tween/TweenGroup.hx
@@ -5,6 +5,8 @@ import k2d.utils.Chain;
 
 class TweenGroup {
     static private inline var EVENT_PROGRESS = "progress";
+    static private inline var EVENT_REPEAT = "repeat";
+    static private inline var EVENT_YOYO = "yoyo";
     static private inline var EVENT_END = "end";
     static private inline var EVENT_PAUSE = "pause";
     static private inline var EVENT_RESUME = "resume";
@@ -13,6 +15,9 @@ class TweenGroup {
     public var yoyo:Bool = false;
     public var repeat:Int = 0;
     public var loop:Bool = false;
+
+    private var _repeat:Int = 0;
+    private var _yoyo:Bool = false;
 
     public var isActive:Bool = false;
     public var isPaused:Bool = false;
@@ -47,9 +52,18 @@ class TweenGroup {
             return;
         }
 
+        _start(inParallel);
+    }
+
+    private function _start(inParallel:Bool, applyYoyo:Bool = false){
         isActive = true;
         _isParallelActive = inParallel;
         _unsubscribeAll = [];
+
+        var tweens = applyYoyo ? this.tweens.copy() : this.tweens;
+        if(applyYoyo){
+            tweens.reverse();
+        }
 
         if(inParallel){
             Chain.each(tweens, _tweenEndCallback, _onEnd);
@@ -71,12 +85,20 @@ class TweenGroup {
             return;
         }
 
+        _stop();
+    }
+
+    private function _stop(reset:Bool = false){
         for(t in tweens){
             t.stop();
             t.reset();
         }
 
         isActive = false;
+        if(reset){
+            _repeat = 0;
+            _yoyo = false;
+        }
 
         for(fn in _unsubscribeAll){
             fn();
@@ -111,6 +133,84 @@ class TweenGroup {
         _eventEmitter.emit(EVENT_RESUME);
     }
 
+    public function subscribeOnProgress(cb:Tween->Void, once:Bool = false){
+        if(once){
+            _eventEmitter.addListenerOnce(EVENT_PROGRESS, cb);
+            return;
+        }
+
+        _eventEmitter.addListener(EVENT_PROGRESS, cb);
+    }
+
+    public function unsubscribeOnProgress(cb:Tween->Void){
+        _eventEmitter.removeListener(EVENT_PROGRESS, cb);
+    }
+
+    public function subscribeOnRepeat(cb:Int->Void, once:Bool = false){
+        if(once){
+            _eventEmitter.addListenerOnce(EVENT_REPEAT, cb);
+            return;
+        }
+
+        _eventEmitter.addListener(EVENT_REPEAT, cb);
+    }
+
+    public function unsubscribeOnRepeat(cb:Int->Void){
+        _eventEmitter.removeListener(EVENT_REPEAT, cb);
+    }
+
+    public function subscribeOnEnd(cb:Void->Void, once:Bool = false){
+        if(once){
+            _eventEmitter.addListenerOnce(EVENT_END, cb);
+            return;
+        }
+
+        _eventEmitter.addListener(EVENT_END, cb);
+    }
+
+    public function unsubscribeOnEnd(cb:Void->Void){
+        _eventEmitter.removeListener(EVENT_END, cb);
+    }
+
+    public function subscribeOnYoyo(cb:Void->Void, once:Bool = false){
+        if(once){
+            _eventEmitter.addListenerOnce(EVENT_YOYO, cb);
+            return;
+        }
+
+        _eventEmitter.addListener(EVENT_YOYO, cb);
+    }
+
+    public function unsubscribeOnYoyo(cb:Void->Void){
+        _eventEmitter.removeListener(EVENT_YOYO, cb);
+    }
+
+    public function subscribeOnPause(cb:Void->Void, once:Bool = false){
+        if(once){
+            _eventEmitter.addListenerOnce(EVENT_PAUSE, cb);
+            return;
+        }
+
+        _eventEmitter.addListener(EVENT_PAUSE, cb);
+    }
+
+    public function unsubscribeOnPause(cb:Void->Void){
+        _eventEmitter.removeListener(EVENT_PAUSE, cb);
+    }
+
+    public function subscribeOnResume(cb:Void->Void, once:Bool = false){
+        if(once){
+            _eventEmitter.addListenerOnce(EVENT_RESUME, cb);
+            return;
+        }
+
+        _eventEmitter.addListener(EVENT_RESUME, cb);
+    }
+
+    public function unsubscribeOnResume(cb:Void->Void){
+        _eventEmitter.removeListener(EVENT_RESUME, cb);
+    }
+
     private function _tweenEndCallback(t:Tween, next:?String->Void){
         var fn = function(){
             _onProgress(t);
@@ -126,7 +226,6 @@ class TweenGroup {
 
     private function _onProgress(tween:Tween) {
         _eventEmitter.emit(EVENT_PROGRESS, tween);
-        trace("on progress");
     }
 
     private function _onEnd(?err:String){
@@ -135,14 +234,21 @@ class TweenGroup {
             return;
         }
 
-        trace("looooooop", this.loop, loop);
-        if(this.loop){
-            stop();
+        if(yoyo && !_yoyo){
+            _yoyo = true;
+            _stop();
+            _start(_isParallelActive, true);
+            _eventEmitter.emit(EVENT_YOYO);
+        }else if(loop || repeat > _repeat){
+            _repeat++;
+            _yoyo = false;
+            _stop();
             start(_isParallelActive);
+            _eventEmitter.emit(EVENT_REPEAT, _repeat);
         }else{
+            _stop(true);
             _eventEmitter.emit(EVENT_END);
         }
-        trace("on end");
     }
 
 }
