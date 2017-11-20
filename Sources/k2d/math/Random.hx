d3ba9f41a61196ba542d2332587c725ad772f998
Nazarí González <nazari.nz@gmail.com>
2017-11-20 18:34:46 +0000

    added some new utils

---
+++ Sources/k2d/math/Random.hx
@@ -0,0 +1,12 @@
+package k2d.math;
+
+/*
+ *  From Kha code.
+ *  Random number generator
+ * 
+ *  Please use this one instead of the native Haxe one to
+ *  keep consistency between different platforms.
+ *     
+ */
+ 
+typedef Random = kha.math.Random;
---
+++ Sources/k2d/utils/Storage.hx
@@ -0,0 +1,3 @@
+package k2d.utils;
+
+//todo storage system
