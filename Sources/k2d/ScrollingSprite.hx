23775a53d59add701d484c61340fece9e2bc84c5
Nazarí González <nazari.nz@gmail.com>
2017-11-28 00:08:04 +0000

    renamed tilingSprite to ScrollingSprite

--- Sources/k2d/ScrollingSprite.hx
+++ Sources/k2d/ScrollingSprite.hx
@@ -5,20 +5,20 @@ import k2d.math.FastFloat;
 import k2d.render.Renderer;
 
 //todo maybe pass a width and height initial to generate a "big" texture(pot2) to use?
-class TilingSprite extends Sprite {
-    public var tileScale:Point = new Point(1,1);
-    public var tilePosition:Point = new Point(0,0);
+class ScrollingSprite extends Sprite {
+    public var scrollScale:Point = new Point(1,1);
+    public var scrollPosition:Point = new Point(0,0);
 
     public override function render(r:Renderer) {
         r.applyTransform(matrixTransform);
 
-        var tpx = tilePosition.x%_texture.width;
-        var tpy = tilePosition.y%_texture.height;
+        var spx = scrollPosition.x%_texture.width;
+        var spy = scrollPosition.y%_texture.height;
 
         //todo
         if(_texture != null){
-            var xLen:Int = Math.ceil((size.x+tpx)/_texture.width);
-            var yLen:Int = Math.ceil((size.y+tpy)/_texture.height);
+            var xLen:Int = Math.ceil((size.x+spx)/_texture.width);
+            var yLen:Int = Math.ceil((size.y+spy)/_texture.height);
 
             for(_y in 0...yLen){
                 for(_x in 0...xLen){
@@ -34,32 +34,32 @@ class TilingSprite extends Sprite {
                     var sh:FastFloat = _texture.height;
 
                     if(_y == 0){
-                        sy = tpy >= 0 ? tpy : tpy+_texture.height;
-                        //trace(sy, _texture.height, tpy, _texture.height-tpy);
+                        sy = spy >= 0 ? spy : spy+_texture.height;
+                        //trace(sy, _texture.height, spy, _texture.height-spy);
                     }else{
-                        yy -= tpy;
+                        yy -= spy;
                         if(yy+_texture.height > size.y){
                             if(_y == yLen-1){
                                 sh = size.y-yy;
                             }else{
-                                sh = size.y%_texture.height+tpy;
+                                sh = size.y%_texture.height+spy;
                             }
                         }
-                        //sh = yy+_texture.height > size.y ? (size.y%_texture.height+tpy)-texture.height : _texture.height;
+                        //sh = yy+_texture.height > size.y ? (size.y%_texture.height+spy)-texture.height : _texture.height;
                     }
 
                     if(_x == 0){
-                        sx = tpx >= 0 ? tpx : tpx+_texture.width;
+                        sx = spx >= 0 ? spx : spx+_texture.width;
                     }else{
-                        xx -= tpx;
+                        xx -= spx;
                         if(xx+_texture.width > size.x){
                             if(_x == xLen-1){
                                 sw = size.x-xx;
                             }else{
-                                sw = size.x%_texture.width+tpx;
+                                sw = size.x%_texture.width+spx;
                             }
                         }
-                        //sw = xx+_texture.width > size.x ? (size.x%_texture.width+tpx) : _texture.width;
+                        //sw = xx+_texture.width > size.x ? (size.x%_texture.width+spx) : _texture.width;
                     }
 
                     r.drawSubTexture(
