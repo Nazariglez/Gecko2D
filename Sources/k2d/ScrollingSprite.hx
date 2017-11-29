b4b96684fd086e51223b76b533156ddfe804a209
Nazarí González <nazari.nz@gmail.com>
2017-11-29 20:25:11 +0000

    scrolling sprite works! (still need some work)

--- Sources/k2d/ScrollingSprite.hx
+++ Sources/k2d/ScrollingSprite.hx
@@ -5,77 +5,116 @@ import k2d.math.FastFloat;
 import k2d.render.Renderer;
 
 //todo maybe pass a width and height initial to generate a "big" texture(pot2) to use?
+//todo do this with a shader instead the render methods
 class ScrollingSprite extends Sprite {
     public var scrollScale:Point = new Point(1,1);
     public var scrollPosition:Point = new Point(0,0);
 
+    private var _scrollX:FastFloat = 0;
+    private var _scrollY:FastFloat = 0;
+    private var _lenX:Int = 0;
+    private var _lenY:Int = 0;
+    private var _offsetX:FastFloat = 0;
+    private var _offsetY:FastFloat = 0;
+
+    private var _xx:FastFloat = 0;
+    private var _yy:FastFloat = 0;
+    private var _sx:FastFloat = 0;
+    private var _sy:FastFloat = 0;
+    private var _sw:FastFloat = 0;
+    private var _sh:FastFloat = 0;
+
+    public override function new(textureName:String, width:FastFloat, height:FastFloat){
+        super(textureName);
+        size.set(width, height);
+    }
+
+    public override function update(dt:FastFloat) {
+        super.update(dt);
+        _updateScrollingInfo();
+    }
+
+    private function _updateScrollingInfo() {
+        _scrollX = scrollPosition.x%_texture.width;
+        _scrollX = _scrollX < 0 ? _texture.width+_scrollX : _scrollX;
+
+        _scrollY = scrollPosition.y%_texture.height;
+        _scrollY = _scrollY < 0 ? _texture.height+_scrollY : _scrollY;
+        
+        _lenX = Math.ceil((size.x + _scrollX)/_texture.width);
+        _lenY = Math.ceil((size.y + _scrollY)/_texture.height);
+
+        _offsetX = size.x-((_lenX*_texture.width)-_scrollX);
+        _offsetY = size.y-((_lenY*_texture.height)-_scrollY);
+    }
+
     public override function render(r:Renderer) {
         r.applyTransform(matrixTransform);
 
-        var spx = scrollPosition.x%_texture.width;
-        var spy = scrollPosition.y%_texture.height;
+        if(_texture != null) {
+            
+            _yy = 0;
+            for(y in 0..._lenY){
+                if(_yy >= size.y){
+                    continue;
+                }
 
-        //todo
-        if(_texture != null){
-            var xLen:Int = Math.ceil((size.x+spx)/_texture.width);
-            var yLen:Int = Math.ceil((size.y+spy)/_texture.height);
+                _xx = 0;
+                _sx = _scrollX;
 
-            for(_y in 0...yLen){
-                for(_x in 0...xLen){
-                    var xx:FastFloat = _x*_texture.width;
-                    var ww:FastFloat = xx+_texture.width;
-                    var yy:FastFloat = _y*_texture.height;
-                    var hh:FastFloat = yy+_texture.height;
+                if(y == 0){
+                    _sy = _scrollY;
+                }else{
+                    _sy = 0;
+                }
 
-                    var sx:FastFloat = 0;
-                    var sy:FastFloat = 0;
+                if(y == _lenY-1) {
+                    _sh = (_texture.height+_offsetY)-_sy;
+                }else{
+                    _sh = _texture.height-_sy;
+                }
 
-                    var sw:FastFloat = _texture.width;
-                    var sh:FastFloat = _texture.height;
-
-                    if(_y == 0){
-                        sy = spy >= 0 ? spy : spy+_texture.height;
-                        //trace(sy, _texture.height, spy, _texture.height-spy);
-                    }else{
-                        yy -= spy;
-                        if(yy+_texture.height > size.y){
-                            if(_y == yLen-1){
-                                sh = size.y-yy;
-                            }else{
-                                sh = size.y%_texture.height+spy;
-                            }
-                        }
-                        //sh = yy+_texture.height > size.y ? (size.y%_texture.height+spy)-texture.height : _texture.height;
+                for(x in 0..._lenX){
+                    if(_xx >= size.x){
+                        continue;
                     }
 
-                    if(_x == 0){
-                        sx = spx >= 0 ? spx : spx+_texture.width;
+                    if(x == _lenX-1){
+                        _sw = (_texture.width+_offsetX)-_sx;
                     }else{
-                        xx -= spx;
-                        if(xx+_texture.width > size.x){
-                            if(_x == xLen-1){
-                                sw = size.x-xx;
-                            }else{
-                                sw = size.x%_texture.width+spx;
-                            }
-                        }
-                        //sw = xx+_texture.width > size.x ? (size.x%_texture.width+spx) : _texture.width;
+                        _sw = _texture.width-_sx;
                     }
 
                     r.drawSubTexture(
-                        _texture, 
-                        xx, 
-                        yy, 
-                        sx, 
-                        sy, 
-                        sw, 
-                        sh
+                        _texture,
+                        _xx, 
+                        _yy,
+                        _sx, 
+                        _sy,
+                        _sw, 
+                        _sh
                     );
+
+                    _xx += _sw;
+                    _sx = 0;
                 }
+
+                _yy += _sh;
             }
+
         }
-        
+
         _renderChildren(r);
         r.applyTransform(matrixTransform);
+    }
+
+    override function set_width(value:FastFloat) : FastFloat {
+        size.x = value / scale.x;
+        return _width = value;
+    }
+
+    override function set_height(value:FastFloat) : FastFloat {
+        size.y = value / scale.y;
+        return _height = value;
     }
 }
