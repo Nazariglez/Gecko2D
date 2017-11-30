b214f991c342606aab09501d53f3b8d1ff3a644e
Nazarí González <nazari.nz@gmail.com>
2017-11-30 00:21:35 +0000

    added a new entitie to create parallax effects

---
+++ Sources/k2d/Parallax.hx
@@ -0,0 +1,10 @@
+package k2d;
+
+import k2d.math.FastFloat;
+
+class Parallax extends Container {
+    //todo parallax
+    public function new(width:FastFloat, height:FastFloat) {
+        size.set(width, height);
+    }
+}
