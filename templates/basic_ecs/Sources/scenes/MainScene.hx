5194c95d37a2790fead28929386a2b57730c0214
Nazarí González <nazari.nz@gmail.com>
2018-02-25 23:32:42 +0000

    added a new template with a loader included

--- templates/basic_ecs/Sources/scenes/MainScene.hx
+++ templates/basic_ecs/Sources/scenes/MainScene.hx
@@ -1,7 +1,20 @@
 package scenes;
 
+import exp.Screen;
+import exp.Entity;
 import exp.Scene;
+import exp.components.core.TransformComponent;
+import exp.components.draw.TextComponent;
 
 class MainScene extends Scene {
+    public function init() {
+        addEntity(_getWelcomeText());
+    }
 
+    private function _getWelcomeText() {
+        var entity = Entity.create();
+        entity.addComponent(TransformComponent.create(Screen.centerX, Screen.centerY));
+        entity.addComponent(TextComponent.create("Welcome to your Gecko2D game!", "Ubuntu-B.ttf", 40));
+        return entity;
+    }
 }
