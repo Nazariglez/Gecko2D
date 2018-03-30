5c0d0f35b6c96128c24ba24d062d7e12fa52d2ac
Nazarí González <nazari.nz@gmail.com>
2018-03-30 23:17:37 +0100

    added some helper functions to manage arrays

--- Sources/gecko/utils/ArrayHelper.hx
+++ Sources/gecko/utils/ArrayHelper.hx
@@ -1,11 +1,15 @@
 package gecko.utils;
 
 class ArrayHelper {
-    static public function clear<T>(arr:Array<T>) {
+    inline static public function clear<T>(arr:Array<T>) {
         #if js
         untyped __js__('{0}.length = 0', arr);
         #else
         arr.splice(0, arr.length);
         #end
+    }
+
+    inline static public function has<T>(arr:Array<T>, obj:T) : Bool {
+        return arr.indexOf(obj) != -1;
     }
 }
