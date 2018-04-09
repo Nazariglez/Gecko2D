3bf53a0459cf5ddc6b3e9ea596374950e85ae703
Nazarí González <nazari.nz@gmail.com>
2018-04-09 22:27:48 +0100

    removed object.hx

--- Sources/gecko/Object.hx
+++
@@ -1,9 +0,0 @@
-package gecko;
-
-import gecko.macros.IAutoPool;
-
-class Object implements IAutoPool {
-    public var id(default, null):Int = Gecko.getUniqueID();
-    public function new(){}
-    public function beforeDestroy(){}
-}
