f9224b5fc8b858a203bcfe846f356b505122ba05
Nazarí González <nazari.nz@gmail.com>
2018-04-24 23:58:05 +0100

    removed "docgen" folder because it's useless

--- docgen/khafile.js
+++
@@ -1,7 +0,0 @@
-let project = new Project('New Project');
-project.addAssets('Assets/**');
-project.addSources('Sources');
-project.addLibrary("/Users/nazarigonzalez/Gecko2D/Sources");
-project.addDefine('--macro include("/Users/nazarigonzalez/Gecko2D/Sources")');
-project.addParameter('-xml docs/gecko.xml');
-resolve(project);
