d96742644f176d0728ee049f1c40cab1bde1337c
Nazarí González <nazari.nz@gmail.com>
2018-04-28 22:07:46 +0100

    removed the old update command

--- cli/commands.ts
+++ cli/commands.ts
@@ -2,7 +2,6 @@ import {Command} from "./cli";
 import * as build from './cmd/build';
 import * as help from './cmd/help';
 import * as init from './cmd/init';
-import * as update from './cmd/update';
 import * as version from './cmd/version';
 import * as serve from './cmd/serve';
 import * as watch from './cmd/watch';
@@ -14,7 +13,6 @@ export const commands:Command[] = [
     build,
     help,
     init,
-    update,
     version,
     serve,
     watch,
